7210b27e75fa609ac75bdb3d36c919058b2f4a71
malikhanab1 <bsai23033@itu.edu.pk>
2025-06-30 02:40:51 +0500

    go fkk urself

---
+++ ex12/script.js
@@ -0,0 +1,14 @@
+console.log("hello")
+
+let b=document.getElementsByClassName("box")
+function getcolor() {
+    let a = Math.floor(Math.random() * 256);
+    let b1 = Math.floor(Math.random() * 256);
+    let c = Math.floor(Math.random() * 256);
+    return `rgb(${a}, ${b1}, ${c})`;
+}
+
+Array.from(b).forEach(a=>{
+    a.style.backgroundColor=getcolor();
+    a.style.color=getcolor();
+})
